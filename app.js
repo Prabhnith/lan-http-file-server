99c42d652a4b726b8cb5769a89548d8729108487
prabhnith <prabhjot.nith@gmail.com>
2017-04-09 23:33:26 +0530

    clean code

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
-var express = require('express');
-var fs = require('fs');
-var app = express();
-
+const express = require('express');
+const fs = require('fs');
+const app = express();
 const dir = require("node-dir");
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.get('/', function(req, res) {
-
 	return res.redirect('/public/home.html');
-
 });
 
 app.use('/public', express.static(__dirname + '/public'));
 
-/** Implementing Simple Music Server using Express JS **/
 app.get('/music', function(req, res) {
-	// File to be served
-
 	var fileId = req.query.id;
 	var file = __dirname + '/music/' + fileId;
 	fs.exists(file, function(exists) {
@@ -28,10 +22,10 @@ app.get('/music', function(req, res) {
 			res.send("Its a 404");
 			res.end();
 		}
-
 	});
 
 });
+
 app.get('/download', function(req, res) {
 	var fileId = req.query.id;
 	var file = __dirname + '/music/' + fileId;
@@ -48,21 +42,19 @@ app.get('/download', function(req, res) {
 	});
 });
 
-server.listen(3030, function() {
-	console.log('App listening on port 3003!');
+server.listen(port, function() {
+	console.log(`Music Server listening on port ${port}...`);
 });
 io.on("connection", function(files) {
 	console.log("received connection");
 	dir.readFiles(__dirname + "/music",
 		function(err, content, next) {
 			if (err) throw err;
-			// console.log('content:', content);
 			next();
 		},
 		function(err, files) {
 			if (err) throw err;
 			console.log('finished reading files:', files);
-			// console.log(files);
 			io.emit("files", {
 				files: files
 			});
