11a508c50d4bea7e827223f20c6e61a753cddbb1
prabhnith <prabhjot.nith@gmail.com>
2017-04-10 13:26:42 +0530

    audio working commit

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const fs = require('fs');
-const app = express();
-const dir = require("node-dir");
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const express 	= require('express');
+const fs  		= require('fs');
+const app 		= express();
+const path 		= require("path");
+const dir 		= require("node-dir");
+const server	= require('http').Server(app);
+const io  		= require('socket.io')(server);
+const port 		= 3030;
 
 app.get('/', function(req, res) {
 	return res.redirect('/public/home.html');
@@ -12,11 +14,11 @@ app.get('/', function(req, res) {
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/music', function(req, res) {
-	var fileId = req.query.id;
-	var file = __dirname + '/music/' + fileId;
+	let fileId = req.query.id;
+	let file = __dirname + '/music/' + fileId;
 	fs.exists(file, function(exists) {
 		if (exists) {
-			var rstream = fs.createReadStream(file);
+			let rstream = fs.createReadStream(file);
 			rstream.pipe(res);
 		} else {
 			res.send("Its a 404");
@@ -27,13 +29,15 @@ app.get('/music', function(req, res) {
 });
 
 app.get('/download', function(req, res) {
-	var fileId = req.query.id;
-	var file = __dirname + '/music/' + fileId;
+	let fileId = req.query.id;
+	console.log("Download  requested : ",fileId);
+	let file = __dirname + '/music/' + fileId;
 	fs.exists(file, function(exists) {
 		if (exists) {
+			console.log(file);
 			res.setHeader('Content-disposition', 'attachment; filename=' + fileId);
 			res.setHeader('Content-Type', 'application/audio/mpeg3')
-			var rstream = fs.createReadStream(file);
+			let rstream = fs.createReadStream(file);
 			rstream.pipe(res);
 		} else {
 			res.send("Its a 404");
@@ -45,18 +49,34 @@ app.get('/download', function(req, res) {
 server.listen(port, function() {
 	console.log(`Music Server listening on port ${port}...`);
 });
+
 io.on("connection", function(files) {
 	console.log("received connection");
-	dir.readFiles(__dirname + "/music",
+	sendFiles();
+});
+
+function sendFiles(){
+dir.readFiles(__dirname + "/music",
 		function(err, content, next) {
 			if (err) throw err;
 			next();
 		},
 		function(err, files) {
 			if (err) throw err;
+			let musicFiles = files.filter( (file) => (path.extname(file)==".mp3" || 
+													  path.extname(file)==".wav" ||
+													  path.extname(file)==".ogg"));
+			// let videoFiles = files.filter( (file) => (path.extname(file)==".mp4" ||
+													  // path.extname(file)==".avi" ||
+													  // path.extname(file)==".webm"));
+
 			console.log('finished reading files:', files);
 			io.emit("files", {
-				files: files
+				musicFiles,
+				// videoFiles
 			});
 		});
-});
+};
+
+
+
